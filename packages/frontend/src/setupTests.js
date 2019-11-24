d8de659ec8c1add2d49c410a92db811be7b05be7
Daniel Cender <danielcender@gmail.com>
2019-11-24 16:51:47 -0600

    chore: adding babel, jest, and enzyme

---
+++ packages/frontend/src/setupTests.js
@@ -0,0 +1,9 @@
+// setup file for Enzyme
+import { configure, render, mount, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
